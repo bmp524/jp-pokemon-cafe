7007de11ea6802cadd81701078a37f4e544b2f15
bmp524 <brittany.cutrona@gmail.com>
2025-04-03 09:50:22 -0400

    # Pokemon cafe reservation booking tool (Tokyo or Osaka) #-----------------------------------------------------
    # Run code to open reservation page. Script will pause for 15 seconds to allow user to pass human test
    # Human check is required every 7-10 min
    
    # Added a location parameter for user to select between Tokyo and Osaka
    # Added text_to_find to output messages to clarify code status
    # Added error handling for advancing calendar month during high traffic
    # Updated XPath date search to only select unique dates (i.e. if future_date.day = 2, do not select previous month's 27 day)
    
    # Successful reservation on 3/21/25

--- Pokemon_Osaka_book_v0.9.py
+++ Pokemon_Osaka_book_v0.9.py
@@ -1,3 +1,23 @@
+# Pokemon cafe reservation booking tool (Tokyo or Osaka)
+#-----------------------------------------------------
+
+# Run code to open reservation page. Script will pause for 15 seconds to allow user to pass human test
+# Human check is required every 7-10 min
+
+# Added a location parameter for user to select between Tokyo and Osaka
+# Added text_to_find to output messages to clarify code status
+# Added error handling for advancing calendar month during high traffic
+# Updated XPath date search to only select unique dates (i.e. if future_date.day = 2, do not select previous month's 27 day)
+
+# Successful reservation on 3/21/25
+
+#-----------------------------------------------------
+
+# User defined parameters
+cafe_location = 'Tokyo' #or 'Osaka'
+num_of_guests = 2 #number of guests for reservation
+sleep_time = 3 #wait time before refresh
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
@@ -9,68 +29,73 @@ from datetime import datetime, time, timedelta
 def restart_program():
     """Restarts the current program."""
     print("Restarting program...")
-    os.execv(sys.executable, ['python'] + sys.argv)
 
 def wait_until_target_time(target_time):
-    """
-    Waits until the specified target time.
-
-    Parameters:
-    target_time (datetime.time): The time to wait until.
-    """
+    # Pause for 15 seconds to allow human interaction with webpage (captcha)
+    # target_time represents now + 15 seconds
+    
     while True:
-        current_time = datetime.now().time()
+        current_time = datetime.now()#.time()
         if current_time >= target_time:
-            print("Target time reached!")
+            print("Time up for human test")
             break
         else:
             # Calculate the time difference
-            current_datetime = datetime.combine(datetime.today(), current_time)
-            target_datetime = datetime.combine(datetime.today(), target_time)
+            current_datetime = datetime.combine(datetime.today(), current_time.time())
+            target_datetime = datetime.combine(datetime.today(), target_time.time())
             time_to_wait = (target_datetime - current_datetime).total_seconds()
             # Sleep for the remaining time
             time_module.sleep(time_to_wait)
 
 
 def refresh_until_found(text_to_find, sleep_time):
+    # Scan webpage for text indicating page can be interacted with, break out of function
+    # Otherwise refresh the page
+
     while True:
         try:
             # Check if the text is present in the page source
             if text_to_find in driver.page_source:
-                print("Text found!")
+                print(text_to_find,"Text found!")
                 break
             else:
-                print("Text not found, refreshing the page...")
+                print(text_to_find,"Text not found, refreshing the page...")
                 driver.refresh()
-                time_module.sleep(sleep_time)  # Wait for 5 seconds before checking again
+                time_module.sleep(sleep_time)  # Wait for sleep_time seconds before checking again
         except NoSuchElementException:
             print("Element not found, refreshing the page...")
             driver.refresh()
-            time_module.sleep(sleep_time)  # Wait for 5 seconds before checking again
+            time_module.sleep(sleep_time)  # Wait for sleep_time seconds before checking again
 
-#The site is congested due to heavy access.
 def refresh_until_not_found(text_to_find, sleep_time):
+    # Scan webpage for text indicating "The site is congested due to heavy access."
+    # Refresh page based on status
+
     while True:
         try:
             # Check if the text is present in the page source
             if text_to_find in driver.page_source:
                 print("The site is congested due to heavy access. found!")
                 driver.refresh()
-                time_module.sleep(sleep_time)  # Wait for 5 seconds before checking again
+                time_module.sleep(sleep_time)  # Wait for sleep_time seconds before checking again
 
             else:
-                print("Text not found, exiting...")
+                print(text_to_find,"Text not found, exiting...")
             break
         except NoSuchElementException:
             print("Element not found, refreshing the page...")
             driver.refresh()
-            time_module.sleep(sleep_time)  # Wait for 5 seconds before checking again
-# Set the target time to 13:59:57
+            time_module.sleep(sleep_time)  # Wait for sleep_time seconds before checking again
 
 
 print("Opening the site main page")
 #
-website = "https://osaka.pokemon-cafe.jp/reserve/step1"
+
+if cafe_location == "Tokyo":
+    website = "https://reserve.pokemon-cafe.jp/reserve/step1"
+elif cafe_location == "Osaka":
+    website = "https://osaka.pokemon-cafe.jp/reserve/step1"
+
 chrome_options = Options()
 # chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -78,18 +103,17 @@ chromedriver = "chromedriver"
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(website)
 
-target_time = time(14, 19, 45)
-print("Waiting until:", target_time)
+target_time = datetime.now() + timedelta(seconds=15) #current time + 15 seconds wait time for human test
+#target_time = datetime.now().time() + timedelta(seconds=15)
+print("Waiting for human input until:", target_time)
 wait_until_target_time(target_time)
+# Allow user to complete human verfication
 
 #checking if "Table Reservation" page successfully opened. If not - refresh the page
-
 # Define the text to find
 text_to_find = "Number of Guests"
-# Wait time before refresh
-sleep_time = 2
-num_of_guests = 2
 
+# Input number of guests using user defined guests parameter
 refresh_until_found(text_to_find, sleep_time)
 #number of guests
 # 席の予約 HTML 3 - Select number of guest
@@ -97,44 +121,58 @@ print("Selecting the number of guests")
 select = Select(driver.find_element(By.NAME, 'guest'))
 select.select_by_index(num_of_guests)
 
+# Check for high congestion page and refresh if necessary
 text_to_find = "The site is congested due to heavy access"
 refresh_until_not_found(text_to_find, sleep_time)
 
-#next month
+# Advance reservation calendar to next month
 print("Selecting the next month")
 #driver.find_element(By.XPATH, "//*[contains(text(), '次の月を見る')]").click()
-driver.find_element(By.CSS_SELECTOR, "div:nth-child(3) > .calendar-pager").click()
 
+while True: 
+    try:
+        if driver.find_element(By.CSS_SELECTOR, "div:nth-child(3) > .calendar-pager").is_displayed() == True:
+            driver.find_element(By.CSS_SELECTOR, "div:nth-child(3) > .calendar-pager").click()
+            print("Next month clicked")
+            break
+        else:
+        #except NoSuchElementException:
+            print("Next month element not found, refreshing the page...")
+            driver.refresh()
+            time_module.sleep(sleep_time)  # Wait for sleep_time seconds before checking again
+    except:
+        print("Error on next month attempt")
+        break
 
+# Check again for high traffic page
 refresh_until_not_found(text_to_find, sleep_time)
 
-#31 from now would be
+# Bookings open 31 days out, search for openings 31 days from today and select date
 future_date = datetime.now() + timedelta(days=31)
 print("The day of the month 31 days from now is:", future_date.day)
-#element = driver.find_element(By.XPATH, "//*[contains(text(), " + str(future_date.day) + ")]")
-#element = driver.find_element(By.XPATH, "//*[contains(@class, 'calendar-day-cell') and contains(text(), " + str(future_date.day) + ")]")
 
-element = driver.find_element(By.XPATH, "//li[contains(@class, 'calendar-day-cell') and contains(., " + str(future_date.day) + ")]")
-
-#driver.execute_script("arguments[0].scrollIntoView();", element)
+#Updated XPath search to only select unique dates (i.e. if future_date.day = 2, do not select previous month's 27 day)
+date_xpath = f"//li[contains(@class, 'calendar-day-cell')]//div[normalize-space(text())='{future_date.day}']"
+element = driver.find_element(By.XPATH, date_xpath)
 element.click()
 
 #driver.find_element(By.XPATH, "//*[contains(text(), " + str(future_date.day) + ")]").click()
 #
 print("Pressing Next step button")
 driver.find_element(By.XPATH, "//*[@class='button' and @id='submit_button']").click()
+
+# Check again for high traffic page
 text_to_find = "The site is congested due to heavy access"
 refresh_until_not_found(text_to_find, sleep_time)
 
 
-
+# Scan for available time slots and refresh if needed
 text_to_find = "Available"
-# Wait time before refresh
-sleep_time = 2
 refresh_until_found(text_to_find, sleep_time)
 
 
 ##########################Available
+# Once available time slot is found, select and check for high traffic if needed
 try:
     # Find the 'div' element containing the text 'Available'
     element = driver.find_element(By.XPATH, "//div[contains(text(), 'Available')]")
@@ -148,57 +186,3 @@ try:
     
 except NoSuchElementException:
     print("Element with text 'Available' not found.")
-
-
-
-
-# Close the WebDriver
-#driver.quit()
-
-#Number of Guests
-
-# def create_booking(day_of_month, num_of_guests, location):
-#   '''Create a reservation for Pokemon Cafe in Tokyo
-#   Keyword arguments:
-#   day_of_month -- day of the month to book
-#   num_of_guests -- number of guests to book (1-8)
-#   '''
-#
-#   if location == "Tokyo":
-#     website = "https://reserve.pokemon-cafe.jp/"
-#   elif location == "Osaka":
-#     website = "https://osaka.pokemon-cafe.jp/"
-#
-#   chrome_options = Options()
-#   # chrome_options = webdriver.ChromeOptions()
-#   chrome_options.add_experimental_option("detach", True)
-#   chromedriver = "chromedriver"
-#   driver = webdriver.Chrome(options=chrome_options)
-#   driver.get(website)
-#
-#   try:
-#     # 席の予約 HTML 1 - Make a reservation
-#     driver.find_element(By.XPATH, "//*[@class='button arrow-down']").click()
-#
-#     # 席の予約 HTML 2 - Agree T&C
-#     driver.find_element(By.XPATH, "//*[@class='agreeChecked']").click()
-#     driver.find_element(By.XPATH, "//*[@class='button']").click()
-#
-#     # 席の予約 HTML 3 - Select number of guest
-#     select = Select(driver.find_element(By.NAME, 'guest'))
-#     select.select_by_index(num_of_guests)
-#
-#     # 席の予約 HTML 4 - Select from calendar
-#     driver.find_element(By.XPATH, "//*[contains(text(), '次の月を見る')]").click()
-#     driver.find_element(By.XPATH, "//*[contains(text(), " + str(day_of_month) + ")]").click()
-#     driver.find_element(By.XPATH, "//*[@class='button']").click()
-#   except NoSuchElementException:
-#     pass
-
-
-#num_iterations = 1
-#day_of_month='01'
-#num_of_guests=2
-#location = 'Tokyo'
-#location = 'Osaka'
-#[create_booking(day_of_month, num_of_guests, location) for x in range(num_iterations)]
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # jp-pokemon-cafe
 This is a time slot booking bot for the Pokémon Café in Japan, inspired by https://medium.com/@patrickgwl/how-to-get-a-pok%C3%A9mon-cafe-reservation-in-japan-daccd1835237 article. The bot is a Python script leveraging Selenium. It can book time slots for both the Tokyo and Osaka Pokémon Café branches.
 
-Bookings open daily at 10 AM Japan time for dates 31 days ahead. New batches of available time slots are released at 10:20 AM and 10:40 AM. The main challenge is that during peak times (10:00, 10:20, 10:40), the café's server often gets overloaded. When this happens, you'll receive a temporary message to refresh the page, and you'll be redirected to the main page.
+The user must edit the user defined parameters for cafe location and number of guests before running. The updated script opens a new webpage, and waits 15 seconds for the user to complete the human test.
+
+Bookings open daily at 6 PM Japan time for dates 31 days ahead. New batches of available time slots are released at 6:20 PM and 6:40 PM (slots can be seen as late as 7:40 PM). The main challenge is that during peak times (18:00, 18:20, 18:40), the café's server often gets overloaded. When this happens, you'll receive a temporary message to refresh the page, and you'll be redirected to the main page.
 
 The script handles this by validating such redirections and repeatedly refreshing the page until you reach the booking page. You have 15 minutes to complete the booking. It's not fully automated; you'll need to manually input your name, email, and other details to complete the reservation. Once done, you'll receive a confirmation email.
 
-Using this approach, I successfully booked a time slot in September 2024.
+Using this approach, I successfully booked a time slot in March 2025.
